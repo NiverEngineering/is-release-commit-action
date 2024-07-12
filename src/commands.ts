595668561f390ab878be9503fbd41cc652f11709
Nicolas Oeschger <nicolas.oeschger@niver.ch>
2024-07-12 14:54:18 +0200

    feat: Create initial version of `is-release-commit-action`

---
+++ src/commands.ts
@@ -0,0 +1,40 @@
+import {spawnCommand} from './spawn';
+
+export const getLatestReleaseTag: (tagPrefix: string, fallback: string) => Promise<string> = async (tagPrefix, fallback) => {
+  let outputOfGitCommand;
+
+  try {
+    outputOfGitCommand = await spawnCommand(
+      'git',
+      'for-each-ref',
+      '--sort=-creatordate',
+      '--count',
+      '1',
+      '--format="%(refname:short)"',
+      `refs/tags/${tagPrefix ?? '*'}`,
+    );
+  } catch (error) {
+    console.error(error);
+    throw Error('Could not get latest tags.');
+  }
+
+  const latestReleaseTag = outputOfGitCommand.stdout?.[0]?.replace(/["\s]/g, '');
+
+  if (latestReleaseTag) {
+    return latestReleaseTag;
+  } else if (fallback) {
+    console.warn(`Could not get latest release release tag. Falling back to '${fallback}'`);
+    return fallback;
+  } else {
+    throw Error('Could not find any matching tag and fallback is not defined!');
+  }
+};
+
+export const isReleaseCommit: (tagPrefix: string) => Promise<boolean> = async (tagPrefix) => {
+  try {
+    return (await spawnCommand('git', 'for-each-ref', '--points-at', 'HEAD', `refs/tags/${tagPrefix}`)).stdout.length > 0;
+  } catch (error) {
+    console.error(error);
+    throw Error('Could not determine whether the current commit is a release commit!');
+  }
+};
