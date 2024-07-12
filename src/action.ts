035e1efc7e3a40a1d56cd5f3a7fac58865c0d946
Nicolas Oeschger <nicolas.oeschger@niver.ch>
2024-07-12 14:54:18 +0200

    feat: Create initial version of `is-release-commit-action`

---
+++ src/action.ts
@@ -0,0 +1,22 @@
+import * as core from '@actions/core';
+import {getLatestReleaseTag, isReleaseCommit} from './commands';
+
+// Main IIFE
+(async () => {
+  try {
+    const tagPrefix = core.getInput('release-tag-prefix');
+    const fallback = core.getInput('fallback-tag');
+
+    const latestTag = await getLatestReleaseTag(tagPrefix, fallback);
+    const releaseCommit = await isReleaseCommit(tagPrefix);
+
+    console.log('Latest Tag:', latestTag);
+    console.log('Is Release Commit:', releaseCommit);
+
+    core.setOutput('latest-release-tag', latestTag);
+    core.setOutput('is-release-commit', releaseCommit);
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+})();
---
+++ src/commands.ts
@@ -0,0 +1,32 @@
+import {spawnCommand} from './spawn';
+
+export const getLatestReleaseTag: (tagPrefix: string, fallback: string) => Promise<string> = async (tagPrefix, fallback) => {
+  let latestReleaseTag;
+
+  try {
+    latestReleaseTag = (
+      await spawnCommand(
+        'git',
+        'for-each-ref',
+        '--sort=-creatordate',
+        '--count',
+        '1',
+        '--format="%(refname:short)"',
+        `refs/tags/${tagPrefix ?? '*'}`,
+      )
+    )?.stdout?.[0]?.replace(/["\s]/g, '');
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (latestReleaseTag) {
+    return latestReleaseTag;
+  } else if (fallback) {
+    return fallback;
+  } else {
+    throw Error('Could not find any matching tag and fallback is not defined!');
+  }
+};
+
+export const isReleaseCommit: (tagPrefix: string) => Promise<boolean> = async (tagPrefix) =>
+  (await spawnCommand('git', 'for-each-ref', '--points-at', 'HEAD', `refs/tags/${tagPrefix}`)).stdout.length > 0;
