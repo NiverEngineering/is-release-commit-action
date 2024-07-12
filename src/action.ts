05e72bc723d97be592a9c562d6d2d751e2d1b875
Nicolas Oeschger <nicolas.oeschger@niver.ch>
2024-07-12 14:54:18 +0200

    feat: Create initial version of `is-release-commit-action`

---
+++ src/action.ts
@@ -0,0 +1,27 @@
+import * as core from '@actions/core';
+import {getLatestReleaseTag, isReleaseCommit} from './commands';
+import {spawnCommand} from './spawn';
+
+// Main IIFE
+(async () => {
+  try {
+    (await spawnCommand('ls', '-lisa')).stdout.forEach((line) => console.log(line));
+    (await spawnCommand('pwd')).stdout.forEach((line) => console.log(line));
+
+    const tagPrefix = core.getInput('release-tag-prefix');
+    const fallback = core.getInput('fallback-tag');
+
+    const latestTag = await getLatestReleaseTag(tagPrefix, fallback);
+    const releaseCommit = await isReleaseCommit(tagPrefix);
+
+    console.log('Latest Tag:', latestTag);
+    console.log('Is Release Commit:', releaseCommit);
+
+    core.setOutput('latest-release-tag', latestTag);
+    core.setOutput('is-release-commit', releaseCommit);
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    console.error(error);
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+})();
